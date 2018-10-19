865c463ecac42f50bfd1c36a34331f5020e29195
Abdullah <abdullah@abdullahs-macbook-pro.local>
2018-10-20 00:40:05 +0300

    Initial Commit

---
+++ VideoPlayerSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayerSwift
+//
+//  Created by Abdullah on 20/10/2018.
+//  Copyright © 2018 Abdullah Alhaider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
